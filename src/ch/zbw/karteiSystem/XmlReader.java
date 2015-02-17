662f7697b11df2a61dd2a3041827dd42a6879b20
jwonagel <jwo.nagel@gmail.com>
2015-02-17 18:32:32 +0100

    finished xml reader start to implement xml reader

--- src/ch/zbw/karteiSystem/XmlReader.java
+++ src/ch/zbw/karteiSystem/XmlReader.java
@@ -3,14 +3,92 @@
  */
 package ch.zbw.karteiSystem;
 
+import java.io.File;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  * @author Jwo
  *
  */
 public class XmlReader {
 	
-	public MainHandler readFile(){
+	private Document doc;
+	private ArrayList<Card> CardList;
+	private ArrayList<CardFile> CardFileList;
+	
+	public MainHandler readFile() throws ParserConfigurationException, SAXException, IOException{
+		read();
+		getData();
 		return null;
 	}
 
+	 private boolean read() throws ParserConfigurationException, SAXException, IOException{
+		 	 CardFileList = new ArrayList<>();
+			 File data = new File("data.bin");
+			 DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			 DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			 doc = dBuilder.parse(data);
+			 doc.getDocumentElement().normalize();
+			 return true;
+	 }
+		
+	 private boolean getData(){
+			 System.out.println("root of xml file" + doc.getDocumentElement().getNodeName());
+			 NodeList nodes = doc.getElementsByTagName("data");
+			 String language ="";
+			 int score =0;
+			 Node node = nodes.item(0);
+			 if (node.getNodeType() == Node.ELEMENT_NODE) {
+			 Element element = (Element) node;
+			 language = getValue("language", element);
+			 score = parseInt(getValue("score", element));
+			 }
+			 System.out.println(language + score);
+			 for(int i =1;i<nodes.getLength();i++){
+				 CardFileList.add(generateCardFileList(nodes.item(i)));
+			 }
+			 
+			 return true;
+			 }
+
+	
+	 private  String getValue(String tag, Element element) {
+			 NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
+			 Node node = (Node) nodes.item(0);
+			 return node.getNodeValue();
+			 }
+			 
+			
+
+
+	 private CardFile generateCardFileList(Node node){
+		 
+		 if (node.getNodeType() == Node.ELEMENT_NODE){
+			 Element element = (Element) node;
+			 
+		 }
+		 return null;
+	 }
+	 
+	 
+	 private int parseInt(String text){
+				try{
+				 return Integer.parseInt(text);
+			} catch(NumberFormatException e){
+				return 0;
+			}
+				
+			}
 }
--- src/ch/zbw/karteiSystem/XmlWriter.java
+++ src/ch/zbw/karteiSystem/XmlWriter.java
@@ -3,15 +3,176 @@
  */
 package ch.zbw.karteiSystem;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import sun.font.CreatedFontTracker;
+
 /**
  * @author Jwo Nagel
- *
+ * 
+ * This class is used for save all the application data into an XML file. for saving call the method save(MainHandler mainhandler)
+ * The data are stored in data.xml
  */
+	
 public class XmlWriter {
 	
+	private Document doc;
+	private Element rootElement;
+	private MainHandler mainHandler;
+	
+	/**
+	 * save all data of the programm into data.xml into the application dir.
+	 * @param mainHandler object filled with the data to save.
+	 * @return true if it worked else false.
+	 */
 	public boolean save(MainHandler mainHandler){
-		return true;
+		boolean worked = false;
+		this.mainHandler=mainHandler;
+		worked=createDoc();
+		if(!worked){
+			return worked;
+		}
+		rootElement = doc.createElement("root");
+		doc.appendChild(rootElement);//generates the root element of the xml.
+		writeAppData();
+		addCardFiles();
+		worked = writeXML();
+		return worked;
 	}
 	
+	/**
+	 * creates a document.
+	 * @return true if it worked.
+	 */
+	private boolean createDoc() {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			return false;
+		}
+		doc = builder.newDocument();
+		return true;
+	}
 
+	/**
+	 * Writes all other program data to the Document.
+	 */
+	private void writeAppData(){
+		Element data = doc.createElement("data");
+		rootElement.appendChild(data);
+		
+		Element language = doc.createElement("language");
+		data.appendChild(language);
+		language.appendChild(doc.createTextNode(mainHandler.getInitLanguage()));
+		
+		Element score = doc.createElement("score");
+		data.appendChild(score);
+		score.appendChild(doc.createTextNode(mainHandler.getScore()+""));
+	}
+	
+	/**
+	 * Writes all CardFiles to the document.
+	 */
+	private void addCardFiles() {
+		ArrayList<CardFile> cardFileList = mainHandler.getAllCardFiles();
+		for(CardFile cardFile : cardFileList){
+			Element cardFileElement = doc.createElement("cardFile");
+			rootElement.appendChild(cardFileElement);
+			
+			Element title = doc.createElement("title");
+			cardFileElement.appendChild(title);
+			title.appendChild(doc.createTextNode(cardFile.getTitle()));
+			
+			Element l1 = doc.createElement("language1");
+			cardFileElement.appendChild(l1);
+			l1.appendChild(doc.createTextNode(cardFile.getLanguage1()));
+			
+			Element l2 = doc.createElement("language2");
+			cardFileElement.appendChild(l2);
+			l2.appendChild(doc.createTextNode(cardFile.getLanguage2()));
+			
+			Element id = doc.createElement("id");
+			cardFileElement.appendChild(id);
+			id.appendChild(doc.createTextNode(cardFile.getCardFileId()+""));
+			
+			Element ls = doc.createElement("languageSwitched");
+			cardFileElement.appendChild(ls);
+			String switched;
+			if(cardFile.isLanguageSwitched()){
+				switched="1";
+			} else{
+				switched="0";
+			}
+			ls.appendChild(doc.createTextNode(switched));
+			
+			addCards(cardFile.getAllCards() ,cardFileElement);
+		}
+		
+	}
+	
+	/**
+	 * adds the elemnts fro Objects Card to the document.
+	 * @param cardList Lists with all cards of a CardFile. 
+	 * @param cardFileElement Element of the given Cardfile.
+	 */
+	private void addCards(ArrayList<Card> cardList, Element cardFileElement) {
+		for(int i=0;i<cardList.size();i++){
+			Element card = doc.createElement("card");
+			card.appendChild(card);
+			
+			Element word1 = doc.createElement("word1");
+			card.appendChild(word1);
+			word1.appendChild(doc.createTextNode(cardList.get(i).getWord1()));
+			
+			Element word2 = doc.createElement("word2");
+			card.appendChild(word2);
+			word2.appendChild(doc.createTextNode(cardList.get(i).getWord2()));
+			
+			Element boxNr = doc.createElement("boxNr");
+			card.appendChild(boxNr);
+			boxNr.appendChild(doc.createTextNode(cardList.get(i).getBoxNr()+""));
+			
+			Element wrongAnswers = doc.createElement("wrongAnswers");
+			card.appendChild(wrongAnswers);
+			wrongAnswers.appendChild(doc.createTextNode(cardList.get(i).getWrongAnswers()+""));
+			
+			Element rightAnswers = doc.createElement("rightAnswers");
+			card.appendChild(rightAnswers);
+			rightAnswers.appendChild(doc.createTextNode(cardList.get(i).getRightAnswers()+""));
+		}
+	}
+	
+	/**
+	 * save the document to the resouces as data.xml.
+	 * @return true if the file is saved.
+	 */
+	private boolean writeXML() {
+		  try {
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("data.bin")); //filename data.xml
+			transformer.transform(source, result);
+			System.out.println("saved");
+		  } catch (TransformerException tfe) {
+			return false;
+		  }
+		  return true;
+		}
 }
