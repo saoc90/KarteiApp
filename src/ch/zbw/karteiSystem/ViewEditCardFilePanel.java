87579aef7754e781ae281cabe4a75e5a724796cf
Martin Thomann <mthomann@me.com>
2015-03-08 10:53:10 +0100

    div changes

--- src/ch/zbw/karteiSystem/ViewEditCardFilePanel.java
+++ src/ch/zbw/karteiSystem/ViewEditCardFilePanel.java
@@ -1,5 +1,6 @@
 package ch.zbw.karteiSystem;
 
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -25,9 +26,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
-
 
 /*
  * @author Martin Thomann
@@ -35,7 +38,7 @@ import javax.swing.table.TableModel;
 
 public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 
-	//Datenfeld
+	// Datenfeld
 	private static final long serialVersionUID = 1L;
 	private static ViewMainFrame viewMainFrame;
 	private Strings strings;
@@ -46,21 +49,23 @@ public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 	private JLabel backButton;
 	private Icon backButtonIcon;
 	private MainHandler mainHandler;
+	private ViewMyTableModel model;
 
 
-	private JTable table;
-	private JScrollPane scrollPane;
-	
-	//Konstruktor
-	public ViewEditCardFilePanel(ViewMainFrame viewMainFrame){
-		
+//	private JTable table;
+//	private JScrollPane scrollPane;
+
+	// Konstruktor
+	public ViewEditCardFilePanel(ViewMainFrame viewMainFrame) {
+
 		this.viewMainFrame = viewMainFrame;
 		strings = viewMainFrame.getStrings();
 		mainHandler = viewMainFrame.getMainHandler();
 		this.setBackground(Color.decode("#52787b"));
 		this.setLayout(new GridBagLayout());
 		gc = new GridBagConstraints();
-		titleBar = new ViewPanelTitleBar(ViewMainFrame.COLOR1,"viewCardFile_title", viewMainFrame);
+		titleBar = new ViewPanelTitleBar(ViewMainFrame.COLOR1,
+				"viewCardFile_title", viewMainFrame);
 		this.setVisible(false);
 		button1 = new JButton();
 		button2 = new JButton();
@@ -71,48 +76,47 @@ public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 		backButtonIcon = new ImageIcon("png/back.png");
 		backButton = new JLabel(backButtonIcon);
 		backButton.addMouseListener(new MouseListener() {
-			
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
+				
 				try {
-					ViewEditCardFilePanel.viewMainFrame.changeFrameTo("toStartPanel");
+					ViewEditCardFilePanel.viewMainFrame.changeFrameTo("toCardFilePanel");
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 		paint();
 	}
-	
 
-	
-	//GUI zeichnen
-	public void paint(){
+	// GUI zeichnen
+	public void paint() {
 		// Title Panel Hinzufügen
 		gc.anchor = GridBagConstraints.PAGE_START;
 		gc.insets = new Insets(20, 0, 0, 0);
@@ -123,7 +127,7 @@ public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 		gc.gridheight = 1;
 		gc.gridwidth = 5;
 		gc.fill = GridBagConstraints.HORIZONTAL;
-		titleBar.changeTitleToJTextField("editTitle");
+		titleBar.changeTitleToJTextField(null);
 		add(titleBar, gc);
 
 		// add(backButton);
@@ -137,17 +141,17 @@ public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 		gc.gridwidth = 1;
 		gc.gridheight = 1;
 		add(backButton, gc);
-		
+
 		gc.anchor = GridBagConstraints.LINE_START;
 		gc.fill = GridBagConstraints.VERTICAL;
-		gc.insets = new Insets(0,0,0,0);
+		gc.insets = new Insets(0, 0, 0, 0);
 		gc.gridx = 1;
 		gc.gridy = 1;
 		gc.gridheight = 1;
 		gc.gridwidth = 4;
 		createTable(null);
-		
-		//Buttons Import/Export hinzufügen
+
+		// Buttons Import/Export hinzufügen
 		gc.fill = GridBagConstraints.NONE;
 		gc.weightx = 1;
 		gc.weighty = 1;
@@ -158,7 +162,7 @@ public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 		gc.gridwidth = 1;
 		gc.gridheight = 1;
 		add(button1, gc);
-		
+
 		gc.fill = GridBagConstraints.NONE;
 		gc.weightx = 2;
 		gc.weighty = 3;
@@ -169,40 +173,36 @@ public class ViewEditCardFilePanel extends JPanel implements ActionListener {
 		gc.gridwidth = 1;
 		gc.gridheight = 1;
 		add(button2, gc);
-		
+
 		setVisible(true);
-		
+
 	}
-	
-	//Tabelle erstellen
-	public void createTable(CardFile cardfile){
-		scrollPane = new JScrollPane();
-		table=new JTable(new ViewMyTableModel(cardfile));
-		
-		scrollPane.setViewportView(table);
-		
-		JPanel p = new JPanel(new GridLayout(0,1,0,20));
+
+	// Tabelle erstellen
+	public void createTable(CardFile cardFile) {
+		model=new ViewMyTableModel(cardFile);
+		JTable table=new JTable(model);
+		JScrollPane scrollPane = new JScrollPane(table);
+
+		JPanel p = new JPanel(new GridLayout(0, 1, 0, 20));
 		p.setBackground(ViewMainFrame.COLOR5);
-		p.setPreferredSize(new Dimension(360,400));
+		p.setPreferredSize(new Dimension(360, 400));
 		p.add(scrollPane);
-		add(p,gc);
+		add(p, gc);
 		setVisible(true);
 	}
 
-	
+
 	public void actionPerformed(ActionEvent e) {
-
-		try {
-			ViewCSVtoTable impTable=new ViewCSVtoTable();
-			table.setModel((TableModel) impTable.readArray());
-			validate();
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if(e.getSource().equals(button1)){
+			model.impTable(viewMainFrame);
 		}
+		if(e.getSource().equals(button2)){
+			model.impTable(viewMainFrame);
+		}
+		
+		
+	
 	}
-	
 
-	
-	
 }
--- src/ch/zbw/karteiSystem/ViewMyTableModel.java
+++ src/ch/zbw/karteiSystem/ViewMyTableModel.java
@@ -1,93 +1,222 @@
 package ch.zbw.karteiSystem;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
-import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
-public class ViewMyTableModel extends AbstractTableModel implements TableModelListener {
-	
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private ViewDatenLieferant daten = new ViewDatenLieferant();
+/*
+ * Die Klasse ViewMyTableModel ist die Verbindung zwischen der JTable und den eigentlichen Daten.
+ * Hier werden die Daten aufbereitet und in der JTable dargestellt.
+ * 
+ * @author Martin Thomann
+ */
 
-	
-	public ViewMyTableModel(CardFile cardFile){
-		daten.setDefaultTable();
+
+public class ViewMyTableModel implements TableModel {
+
+	private CardFile cardFile;
+	private ArrayList<Card> wordListCard;
+	private String titel;
+	private String language1 = "Sprache1";
+	private String language2 = "Sprache2";
+	private int id;
+	private boolean languageSwitched;
+	private static ViewMainFrame viewMainFrame;
+
+	@SuppressWarnings("rawtypes")
+	private Vector wordListJTable=new Vector();
+	@SuppressWarnings("rawtypes")
+	private Vector listener = new Vector();
+
+	//Konstruktor - Diesem wird entweder ein CardFile oder null übergeben.
+	//Wenn Null übergeben wird, wird eine neue Tabelle erzeugt, ansonsten die CardFile ausgelesen.
+	public ViewMyTableModel(CardFile cardFile) {
+		this.cardFile = cardFile;
+		if (cardFile != null) {
+			readCardFile();
+		} else {
+			createNewTable();
+		}
 	}
+
+	//Das übergebene CardFile wird ausgelesen und in die Datenfelder gespeichert.
+	@SuppressWarnings("unchecked")
+	public void readCardFile() {
+		titel = cardFile.getTitle();
+		language1 = cardFile.getLanguage1();
+		language2 = cardFile.getLanguage2();
+		id = cardFile.getCardFileId();
+		wordListCard = cardFile.getAllCards();
+		
+		wordListJTable.clear();
+		int c = 0;
+		while (c <= wordListCard.size()) {
+			String tempWord1=wordListCard.get(c).getWord1();
+			String tempWord2=wordListCard.get(c).getWord1();
+			ViewEditCardFileWords words= new ViewEditCardFileWords(tempWord1, tempWord2);
+			wordListJTable.add(words);
+			c++;
+		}
+		
+		TableModelEvent e = new TableModelEvent(this);
+
+		// Nun das Event verschicken
+		for (int i = 0, n = listener.size(); i < n; i++) {
+			((TableModelListener) listener.get(i)).tableChanged(e);
+		}
+	}
+
+	//Es wird eine neue Tabelle mit 20 Zeilen erstellt
+	@SuppressWarnings("unchecked")
+	public void createNewTable() {
+		wordListJTable.clear();
+		for(int i=0; i<=200; i++){
+			String tempWord1="A"+i;
+			String tempWord2="B"+i;
+			ViewEditCardFileWords words= new ViewEditCardFileWords(tempWord1, tempWord2);
+			wordListJTable.add(words);
+		}
+
+		TableModelEvent e = new TableModelEvent(this);
+
+		// Nun das Event verschicken
+		for (int i = 0, n = listener.size(); i < n; i++) {
+			((TableModelListener) listener.get(i)).tableChanged(e);
+		}
+	}
+	
+	//Das gespeicherte CardFile wird mit den neuen veränderten Werte gespeichert.
+	public CardFile updatedCardFile(){
+		cardFile.setTitle(titel);
+		cardFile.setLanguage1(language1);
+		cardFile.setLanguage2(language2);
+		wordListCard.clear();
+		Card tempCard;
+		for(int i=0; i<=wordListJTable.size(); i++){
+			ViewEditCardFileWords tempWords=(ViewEditCardFileWords) wordListJTable.get(i);
+			String tempWord1 = tempWords.getWord1();
+			String tempWord2 = tempWords.getWord2();
+			tempCard=new Card(tempWord1, tempWord2);
+			wordListCard.add(tempCard);
+		}
+		return cardFile;
+	}
+	
+	//Eine CSV Wörter-Tabelle wird importiert. Diese wird in die wordListJTable gespeichert.
+	@SuppressWarnings("unchecked")
+	public void impTable(ViewMainFrame viewMainFrame){
+		wordListJTable.clear();
+		ImportCSV imp=new ImportCSV();
+		try {
+			ArrayList<ArrayList<String>> tempWordListAL=imp.importList(viewMainFrame);
+			ArrayList<String> tempWordList1=tempWordListAL.get(0);
+			ArrayList<String> tempWordList2=tempWordListAL.get(0);
+			
+			for(int i=0; i<=tempWordList1.size(); i++){
+				String tempWord1 = tempWordList1.get(i);
+				String tempWord2 = tempWordList2.get(i);
+				ViewEditCardFileWords tempWords = new ViewEditCardFileWords(tempWord1, tempWord2);
+				wordListJTable.add(tempWords);
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		TableModelEvent e = new TableModelEvent(this);
+
+		// Nun das Event verschicken
+		for (int i = 0, n = listener.size(); i < n; i++) {
+			((TableModelListener) listener.get(i)).tableChanged(e);
+		}
+	}
+	
+	//Die wordListJTable wird als CSV exportiert.
+//	public void expTable(ViewMainFrame viewMainFrame){
+//		ExportCSV exp=new ExportCSV();
+//		try {
+//			exp.exportList(wordListJTable, viewMainFrame);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//	
+	
+	
 
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 200;
+		return wordListJTable.size();
 	}
 
-	
-	@Override
-    public Object getValueAt(int zeile, int spalte) { // Die eigentlichen Daten
-        if (spalte==0) return daten.getSprache1(zeile);
-        if (spalte==1) return daten.getSprache2(zeile);
-        return null;
-    }
-	
-	
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
 		return 2;
 	}
-	
-	
+
 	@Override
-    public String getColumnName(int arg0) {    // Die Spaltenueberschriften
-        if (arg0==0) return "Sprache1";
-        if (arg0==1) return "Sprache2";
-        return null;
-    }
+	public String getColumnName(int columnIndex) {
+
+		switch (columnIndex) {
+		case 0:
+			return language1;
+		case 1:
+			return language2;
+		default:
+			return null;
+		}
+	}
 
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		// TODO Auto-generated method stub
-	    return String.class;
+		return String.class;
 	}
 
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		ViewEditCardFileWords words=(ViewEditCardFileWords) wordListJTable.get(rowIndex);
+		switch(columnIndex){
+		case 0: return words.getWord1();
+		case 1: return words.getWord2();
+		default: return null;
+		}
+	}
+
+	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		
+		ViewEditCardFileWords tempWords = (ViewEditCardFileWords)wordListJTable.get( rowIndex );
+		 
+		switch( columnIndex ){
+			case 0: 
+				tempWords.setWord1( (String)aValue );
+				break;
+			case 1: 
+				tempWords.setWord2( (String)aValue );
+				break;
+		}
+	
 	}
 
 	@Override
 	public void addTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-		
+		listener.add(l);
+
 	}
 
 	@Override
 	public void removeTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-		
-	}
+		listener.remove(l);
 
-	@Override
-	public void tableChanged(TableModelEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
-	
-	
-	
-	
-	
-	
-	
-
 
 }
