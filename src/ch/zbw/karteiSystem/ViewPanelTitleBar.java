e394d547c5d90e0810b7bbb58e3addb6e977f56f
Martin Thomann <mthomann@me.com>
2015-03-11 18:26:14 +0100

    div

--- src/ch/zbw/karteiSystem/ViewPanelTitleBar.java
+++ src/ch/zbw/karteiSystem/ViewPanelTitleBar.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -56,6 +58,38 @@ public class ViewPanelTitleBar extends JPanel {
 		add(jtf, gc);
 		strings.add(jtf);
 		this.jtf.setText(strings.getString("editTitle"));
+		
+		jtf.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				jtf.setText("");
+			}
+		});
 	}
 	
 	public void changeTitleJTextField(String name){
